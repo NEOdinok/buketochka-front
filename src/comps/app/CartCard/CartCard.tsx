f0ada5963d251a87b485b2492b26aa4f7e12795b
goshamartynovich <goshamartynovich@gmail.com>
2023-08-13 21:43:43 +0200

    + placeholderImg constant for CartCard

--- src/comps/app/CartCard/CartCard.tsx
+++ src/comps/app/CartCard/CartCard.tsx
@@ -8,13 +8,13 @@ import { productType, imagesDataType } from '@/types';
 import { useEffect } from 'react';
 import CartStore from '@/stores/CartStore';
 import { observer } from 'mobx-react-lite';
+import { placeholderImg } from './cart.contants';
 
 interface Props {
 	product: productType,
 }
 
 const CartCard: React.FC<Props> = ({ product }) => {
-	const placeholderImg = "https://firebasestorage.googleapis.com/v0/b/buketochka-quasar-backend.appspot.com/o/images%2Fb467ea5a-4538-4b9b-b0e0-9f1c8ae6f3b5.jpg?alt=media&token=91c2ff69-ed08-45db-a945-61e6dfdf58b7"
 
 	return (
 		<div className={styles.cartCard}>
---
+++ src/comps/app/CartCard/cart.contants.ts
@@ -0,0 +1,3 @@
+const placeholderImg = "https://firebasestorage.googleapis.com/v0/b/buketochka-quasar-backend.appspot.com/o/images%2Fb467ea5a-4538-4b9b-b0e0-9f1c8ae6f3b5.jpg?alt=media&token=91c2ff69-ed08-45db-a945-61e6dfdf58b7"
+
+export { placeholderImg }
